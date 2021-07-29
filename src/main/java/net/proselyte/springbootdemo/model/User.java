eee903043a1bfae41e62fbbf23ed68f9f7d6d79e
777AMR <meyram.ramazanov@mail.ru>
2021-07-29 21:12:48 +0600

    CRUD app.

---
+++ src/main/java/net/proselyte/springbootdemo/model/User.java
@@ -0,0 +1,19 @@
+package net.proselyte.springbootdemo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+}
